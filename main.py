f2ac6fcc7b0bbb908f266b8c3736f771a9c58df7
aaix <git@aaix.pro>
2020-11-08 17:34:12 +0000

    idk

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ class EchoServerProtocol(asyncio.Protocol):
         self.transport = transport
         self.peername = transport.get_extra_info('peername')
         self.peername = f"{self.peername[0]}-{self.peername[1]}"
-        print(f"Connection from {self.peername}")
+        print(f"Connection from {self.peername} [{len(clients)+1}]")
         clients.append(transport)
 
     def connection_lost(self,exec):
         clients.remove(self.transport)
-        print(f"Closing {self.peername}")
+        print(f"Closing {self.peername} [{len(clients)}]")
 
 
 async def main():
     loop = asyncio.get_running_loop()
-    server = await loop.create_server(EchoServerProtocol,'127.0.0.1', 9455)
+    server = await loop.create_server(EchoServerProtocol,'0.0.0.0', 9455)
 
     async with server:
         await server.start_serving()
         while True:
             input = await ainput("")
-            for client in clients:
-                client.write(bytes(input,encoding="utf-8"))
+            if len(clients) > 0:
+                print(f"Sending to {len(clients)}")
+                for client in clients:
+                    client.write(bytes(input,encoding="utf-8"))
 
 
 asyncio.run(main())
